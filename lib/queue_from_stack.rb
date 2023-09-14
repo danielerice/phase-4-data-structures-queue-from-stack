d180453f1fa363737567d2dee8d4345bca07c693
Daniel Rice <danielrice5627@gmail.com>
2023-09-14 10:42:43 -0700

    commit tree 42aa903aa83283c5d3ecebf2c2fd50438d2861bb

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,41 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+
+    def initialize
+        @s1 = Stack.new
+        @s2 = Stack.new
+    end
+
+    def add value
+        @s1.push value
+    end
+
+    def remove
+        if @s2.empty?
+            until @s1.empty?
+                @s2.push(@s1.pop)
+            end
+        end
+        return @s2.pop
+    end
+
+    def peek
+        if @s2.empty?
+            until @s1.empty?
+                @s2.push(@s1.pop)
+            end
+        end
+        return @s2.peek
+    end
+
+    def empty?
+        if @s2.empty? && @s1.empty?
+            return true
+        else
+            return false
+        end
+    end
+
+
+end
